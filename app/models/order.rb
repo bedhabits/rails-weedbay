57d599d30e67a00caa086014a71631556544292a
Radu Popescu <radu.popescu@priceedge.se>
2020-02-13 11:19:03 +0000

    added model validations & db fields constraints

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,9 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :product_orders
+  has_many :reviews
+  has_many :products, through: :product_orders
+
+  validates :status, presence: true, inclusion: { in: %w(open closed), message: "%{value} is not a valid order status" }
+  validates :address, presence: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ApplicationRecord
   belongs_to :user
+  has_many :product_orders
+  has_many :orders, through: :product_orders
+  has_many :reviews, through: :orders
+
+  validates :name, presence: true
+  validates :origin, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :type, inclusion: { in: %w(sativa indica hybrid ruderalis), message: "%{value} is not a valid weed type" }
 end
