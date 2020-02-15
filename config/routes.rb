abf3d684d8caaeba58329223c698a893065056a4
Philip OKa <philip.oka@gmail.com>
2020-02-15 15:08:51 +0000

    created the orders controller with pundit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'products#index'
-  namespace :owner do
-    resources :products
-  end
-  resources :products, only: %i[index show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  resources :products
+  resources :orders
 end
