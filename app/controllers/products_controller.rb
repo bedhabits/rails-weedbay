1b772518af8340f6334ac2b52f7e3083af4f34fc
Radu Popescu <radu.popescu@priceedge.se>
2020-02-15 12:46:03 +0000

    pundit almost done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,56 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
-    @products = Product.all
+    # @products = Product.all
+    @products = policy_scope(Product)
+  end
+
+  def new
+    @product = Product.new
+    authorize @product
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to product_path(@product), notice: 'Product was updated'
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.user = current_user
+    authorize @product
+
+    if @product.save
+      redirect_to @product, notice: 'Product was created'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to root_path, notice: 'Product was deleted'
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :origin, :price, :weed_type)
+  end
+
+  def set_product
     @product = Product.find(params[:id])
+    authorize @product
   end
 end
