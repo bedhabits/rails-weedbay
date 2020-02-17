9d257e1f2c44770d4e7bd8241d2f680a40dccd4b
bernardo goes <bernardogoes@gmail.com>
2020-02-17 16:06:48 +0000

    trying to fix the username and photo upload fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
     # For additional fields in app/views/devise/registrations/new.html.erb
     # devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update)
+    devise_parameter_sanitizer.permit(:account_update, :username)
   end
   private
   def skip_pundit?
