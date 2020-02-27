483f24601c0cc5a637d479935718d5651445b985
Radu Popescu <radu.popescu@priceedge.se>
2020-02-27 19:35:58 +0000

    done

--- app/models/product.rb
+++ app/models/product.rb
@@ -16,15 +16,7 @@ class Product < ApplicationRecord
 
   include PgSearch::Model
   pg_search_scope :product_search,
-    against: {
-      price: 'A',
-      weed_type: 'B',
-      name: 'C',
-      origin: 'D',
-      effects: 'E',
-      ailment: 'F',
-      flavor: 'G'
-    },
+    against: [:price, :weed_type, :name, :origin, :effects, :ailment, :flavor],
     associated_against: {
       user: [ :username, :email ]
     },
